a72c7e988f695aa1d9c813e2a877c56c98a6be05
boojo <jshim27@wisc.edu>
2025-09-13 18:46:12 -0500

    Quartz sync: Sep 13, 2025, 6:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -30,9 +30,9 @@ const config: QuartzConfig = {
           highlight: "rgba(143, 159, 169, 0.15)",
         },
         darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
+          light: "#09090b",
+          lightgray: "#27272A",
+          gray: "#3F3F46",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
           secondary: "#7b97aa",
